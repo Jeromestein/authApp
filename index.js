9de874cf80f4ff6cd6365d530c5e66470e4125da
Jerome <nietsemorej@gmail.com>
2025-08-04 15:22:27 -0700

    Add user registration for 'paul', 'joy', and 'ray' in index.js; create styles.css for basic styling and layout

--- index.js
+++ index.js
@@ -77,3 +77,8 @@ app.get('/logout', (req, res) => {
     req.logout();
     res.sendFile('html/logout.html', { root: __dirname });
 });
+
+/* REGISTER SOME USERS */
+UserDetails.register({username: 'paul', active: false}, 'paul');
+UserDetails.register({username: 'joy', active: false}, 'joy');
+UserDetails.register({username: 'ray', active: false}, 'ray');
